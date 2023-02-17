27fc761da8ca704da1079ac10da38f8dba6f3f75
ccm <ccm@iMac-OSX.local>
2023-02-17 00:59:10 -0500

    1st Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+  "author": "Christian Cattani"
+  "description": "Parcel con Jquery, Bootstrap para proyectos simples"
+
+  Instalación:
+  npm install o yarn install
+
+  dev:
+  npm run start / yarn start
+
+  build:
+  npm run build / yarn build
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import * as bootstrap from "bootstrap";
+import $ from "jquery";
+
+// import imageURL from './assets/trollHuntersArcadia.png';
+
+// JQuery
+$(function () {
+  // bootstrap tooltips
+
+    $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" });
+
+});
